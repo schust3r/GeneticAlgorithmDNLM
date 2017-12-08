8a38cbe63bef8b8d7d8c647fc2436f4936f2f58b
J. Schuster <joe.schust@gmail.com>
2017-12-07 21:36:21 -0600

    Added unit test for DNLM filter and dice result

---
+++ src/com/parma/filter/FilterResultTest.java
@@ -0,0 +1,47 @@
+package com.parma.filter;
+
+import org.junit.Test;
+import org.opencv.core.Mat;
+import com.parma.genetics.scoring.Dice;
+import com.parma.images.ImageHandler;
+import com.parma.segmentation.Thresholder;
+
+public class FilterResultTest {
+
+  @Test
+  public void testFilter() {
+
+    ImageHandler ih = new ImageHandler();
+
+    String filename = "002";
+    int w = 3;
+    int w_n = 5;
+    int sigma_r = 300;
+
+    Mat imagen = ih.leerImagenGrises("images/" + filename + ".png");
+    Mat groundtruth = ih.leerImagenGrises("images/" + filename + "_gt.png");
+
+    // apply filter
+    Mat res = DnlmFilter.filter(imagen, w, w_n, sigma_r);
+
+    // cut unwanted borders
+    int snip = w + w_n;
+    res = res.submat(snip, res.rows() - snip - 2, snip, res.cols() - snip - 2);
+    groundtruth = groundtruth.submat(snip, groundtruth.rows() - snip - 2, snip,
+        groundtruth.cols() - snip - 2);
+
+    // apply binarization + Otsu thresholding
+    Thresholder th = new Thresholder();
+    th.applyOtsuThreshold(res);
+    th.applyThreshold(groundtruth, 1);        
+
+    // calculate dice similarity index
+    double dice = Dice.calcularDice(res, groundtruth);
+
+    ih.guardarImagen("images/", filename + "_out", "png", res);
+    
+    System.out.println(dice);
+
+  }
+
+}
