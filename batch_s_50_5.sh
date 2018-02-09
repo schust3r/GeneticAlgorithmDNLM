2463eea0cf5375092c08a9fbf38c39965a32607a
Ubuntu <jdnlm@Ubuntu14CPU8.Ubuntu14CPU84221.g6.internal.cloudapp.net>
2018-02-09 03:34:14 +0000

    New results

---
+++ batch_s_50_5.sh
@@ -0,0 +1,13 @@
+java -Djava.library.path=x64 -jar gadnlm.jar s 50 5 >> results/out_s_50_5_r1.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 50 5 >> results/out_s_50_5_r2.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 50 5 >> results/out_s_50_5_r3.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 50 5 >> results/out_s_50_5_r4.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 50 5 >> results/out_s_50_5_r5.txt
