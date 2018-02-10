fa16174d8fff32b87b5292043e089e448f25f615
Ubuntu <jdnlm@Ubuntu14CPU8.Ubuntu14CPU84221.g6.internal.cloudapp.net>
2018-02-10 20:32:47 +0000

    Multiple crossover finished

---
+++ batch_c_50_5.sh
@@ -0,0 +1,13 @@
+java -Djava.library.path=x64 -jar gadnlm.jar c 50 5 >> results/out_m_50_5_r1.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar c 50 5 >> results/out_m_50_5_r2.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar c 50 5 >> results/out_m_50_5_r3.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar c 50 5 >> results/out_m_50_5_r4.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar c 50 5 >> results/out_m_50_5_r5.txt
