63d8f5683db39292da31daf1b035049a33472822
Ubuntu <jdnlm@jdnlm.jdnlm4584.j3.internal.cloudapp.net>
2018-03-02 19:30:21 +0000

    New results

---
+++ batch_10i_10g.sh
@@ -0,0 +1,15 @@
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 10 >> results/ga_10i_10g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 10 >> results/ga_10i_10g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 10 >> results/ga_10i_10g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 10 >> results/ga_10i_10g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 10 >> results/ga_10i_10g.txt
+sleep 30
+
