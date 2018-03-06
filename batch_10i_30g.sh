2dec0059f6b7e9dc25571c6a0970ca97aeb2161a
Ubuntu <jdnlm@jdnlm.jdnlm4584.j3.internal.cloudapp.net>
2018-03-06 15:58:46 +0000

    New results

---
+++ batch_10i_30g.sh
@@ -0,0 +1,15 @@
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 30 >> results/ga_10i_30g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 30 >> results/ga_10i_30g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 30 >> results/ga_10i_30g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 30 >> results/ga_10i_30g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 30 >> results/ga_10i_30g.txt
+sleep 30
+
---
+++ batch_25i_30g.sh
@@ -0,0 +1,15 @@
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 30 >> results/ga_25i_30g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 30 >> results/ga_25i_30g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 30 >> results/ga_25i_30g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 30 >> results/ga_25i_30g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 30 >> results/ga_25i_30g.txt
+sleep 30
+
