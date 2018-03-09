51d4ae2cd23a387c4ffdd96a20974ad89213e04b
J. Schuster <joe.schust@gmail.com>
2018-03-08 23:32:12 -0600

    Reorg

---
+++ batch_10i_25g.sh
@@ -0,0 +1,28 @@
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 25 >> results/08-03-2018/ga_10i_25g.txt
