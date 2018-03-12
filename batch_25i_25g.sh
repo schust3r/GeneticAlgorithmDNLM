181e474f41651747fbeb27f3cbea6486ae139cea
Ubuntu <jdnlm@jdnlm3.jdnlm36202.j8.internal.cloudapp.net>
2018-03-12 02:32:30 +0000

    Results

--- batch_25i_25g.sh
+++ batch_25i_25g.sh
@@ -1,28 +1,29 @@
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_1.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_2.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_3.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_4.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_5.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_6.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_7.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_8.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_9.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g.txt
+java -Djava.library.path=x64 -jar gadnlm.jar s 25 5 25 >> results/08-03-2018/ga_25i_25g_10.txt
+sleep 30
