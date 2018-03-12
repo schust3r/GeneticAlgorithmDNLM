cbf81af960395f6ed3d74267f067f36be3388998
Ubuntu <jdnlm@MVLinux.dnlm.j2.internal.cloudapp.net>
2018-03-12 02:24:13 +0000

    Results so far

---
+++ batch_ga_15i_25g.sh
@@ -0,0 +1,28 @@
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_1.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_2.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_3.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_4.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_5.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_6.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_7.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_8.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_9.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 15 5 25 >> results/11-03-2018/ga_15i_25g_10.txt
