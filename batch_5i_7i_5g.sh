f81d309aa3c12e34ab38f4eddb3f79ac92de51ec
Ubuntu <jdnlm@jdnlm3.jdnlm36202.j8.internal.cloudapp.net>
2018-03-12 19:14:13 +0000

    results added

---
+++ batch_5i_7i_5g.sh
@@ -0,0 +1,59 @@
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_1.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_2.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_3.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_4.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_5.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_6.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_7.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_8.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_9.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 5 5 5 >> results/ga_5i_5g_10.txt
+
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_1.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_2.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_3.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_4.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_5.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_6.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_7.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_8.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_9.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 7 5 5 >> results/ga_7i_5g_10.txt
