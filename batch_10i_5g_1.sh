557e4b2b4b7c76bb1000481e36d9b1eaba87be54
Joel Barrantes <joel.bg95@outlook.com>
2018-03-14 00:03:00 -0600

    The absolute commitman

---
+++ batch_10i_5g_1.sh
@@ -0,0 +1,14 @@
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_1.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_2.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_3.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_4.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_5.txt
---
+++ batch_10i_5g_2.sh
@@ -0,0 +1,14 @@
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_6.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_7.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_8.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_9.txt
+sleep 30
+
+java -Djava.library.path=x64 -jar gadnlm.jar s 10 5 5 >> results/ga_10i_5g_10.txt
